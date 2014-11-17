3a50ddd7355bd359b407c63d68bdb0ff313091c8
Ronny Ager-Wick <ronny@ager-wick.com>
2014-11-17 19:49:54 +0800

    Added follow_redirect! in the notes request test

--- test/requests/test_notes.rb
+++ test/requests/test_notes.rb
@@ -5,6 +5,7 @@ describe App do
   describe "notes page" do
     it "should add a new note" do
       post '/notes', :content => 'My new note'
+      follow_redirect!
       last_response.must_be_ok
       last_response.body.must_include 'My new note'
     end
