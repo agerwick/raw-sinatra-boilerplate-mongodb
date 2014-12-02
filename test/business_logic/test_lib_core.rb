033a4f8b294a894cfe098bcae12c1169a5ff1b6d
Ronny Ager-Wick <ronny@ager-wick.com>
2014-12-02 13:08:32 +0800

    Added to_boolean method

--- test/business_logic/test_lib_core.rb
+++ test/business_logic/test_lib_core.rb
@@ -34,4 +34,44 @@ describe App do
     end
   end
 
+  describe "convert to boolean" do
+    it "should handle boolean" do
+      false.to_boolean.must_equal false
+      true.to_boolean.must_equal true
+    end
+    it "should handle numbers, any positive non-zero number is true, enything else is false" do
+      -1.to_boolean.must_equal false
+      0.to_boolean.must_equal false
+      1.to_boolean.must_equal true
+      0.0000001.to_boolean.must_equal true
+      -13.5.to_boolean.must_equal false
+      -0.2.to_boolean.must_equal false
+      8.333.to_boolean.must_equal true
+    end
+    it "should handle strings, 'true', 'yes' and '1' is true, anything else is false" do
+      "".to_boolean.must_equal false
+      "false".to_boolean.must_equal false
+      "XYZTRUEXYZ".to_boolean.must_equal false
+      "true!".to_boolean.must_equal false
+      "0".to_boolean.must_equal false
+      "1".to_boolean.must_equal true
+      "true".to_boolean.must_equal true
+      "True".to_boolean.must_equal true
+      "truE".to_boolean.must_equal true
+      "TRUE".to_boolean.must_equal true
+      "yes".to_boolean.must_equal true
+      "Yes".to_boolean.must_equal true
+      "yEs".to_boolean.must_equal true
+      "YES".to_boolean.must_equal true
+    end
+    it "should handle arrays and hashes, empty is false, anything else is true" do
+      [].to_boolean.must_equal false
+      ['a'].to_boolean.must_equal true
+      [''].to_boolean.must_equal true
+      {}.to_boolean.must_equal false
+      {:a => 'a'}.to_boolean.must_equal true
+      {false => false}.to_boolean.must_equal true
+    end
+  end
+
 end
